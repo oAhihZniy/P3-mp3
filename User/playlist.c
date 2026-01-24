715c035f8e17785d9b93f15a0aa4cf437106c4c7
shelter <shelter_yin@outlook.com>
2026-01-24 17:01:53 +0800

    文件列表

---
+++ User/playlist.c
@@ -0,0 +1,103 @@
+#include "playlist.h"
+#include "audio_driver.h"
+#include <string.h>
+
+Playlist_t g_playlist = {0, 0, ""};
+
+/**
+ * 扫描根目录所有 .mp3 文件，统计总数
+ */
+FRESULT Playlist_Init(void) {
+    DIR dir;
+    FILINFO fno;
+    FRESULT res;
+
+    g_playlist.total_count = 0;
+    res = f_opendir(&dir, "/"); // 打开根目录
+    if (res != FR_OK) return res;
+
+    while (1) {
+        res = f_readdir(&dir, &fno);
+        if (res != FR_OK || fno.fname[0] == 0) break; // 错误或结束
+
+        // 检查后缀是否为 .mp3
+        if (!(fno.fattrib & AM_DIR)) { // 不是目录
+            char *fn = fno.fname;
+            int len = strlen(fn);
+            if (len > 4 && strcasecmp(fn + len - 4, ".mp3") == 0) {
+                g_playlist.total_count++;
+                if (g_playlist.total_count >= MAX_SONGS) break;
+            }
+        }
+    }
+    f_closedir(&dir);
+    return FR_OK;
+}
+
+/**
+ * 根据索引号在 SD 卡中寻找文件名
+ */
+FRESULT Playlist_GetFileName(uint16_t index, char* out_name) {
+    DIR dir;
+    FILINFO fno;
+    FRESULT res;
+    uint16_t count = 0;
+
+    res = f_opendir(&dir, "/");
+    if (res != FR_OK) return res;
+
+    while (1) {
+        res = f_readdir(&dir, &fno);
+        if (res != FR_OK || fno.fname[0] == 0) break;
+
+        if (!(fno.fattrib & AM_DIR)) {
+            int len = strlen(fno.fname);
+            if (len > 4 && strcasecmp(fno.fname + len - 4, ".mp3") == 0) {
+                if (count == index) {
+                    strcpy(out_name, fno.fname);
+                    f_closedir(&dir);
+                    return FR_OK;
+                }
+                count++;
+            }
+        }
+    }
+    f_closedir(&dir);
+    return FR_NO_FILE;
+}
+
+void Playlist_PlayCurrent(void) {
+    if (Playlist_GetFileName(g_playlist.current_index, g_playlist.current_filename) == FR_OK) {
+        Audio_Play(g_playlist.current_filename);
+    }
+}
+
+void Playlist_Next(void) {
+    g_playlist.current_index++;
+    if (g_playlist.current_index >= g_playlist.total_count) {
+        g_playlist.current_index = 0; // 回到第一首
+    }
+    Playlist_PlayCurrent();
+}
+
+void Playlist_Prev(void) {
+    if (g_playlist.current_index == 0) {
+        g_playlist.current_index = g_playlist.total_count - 1;
+    } else {
+        g_playlist.current_index--;
+    }
+    Playlist_PlayCurrent();
+}
+
+/**
+ * 核心任务：检查歌曲是否自然播放结束
+ * 放在 main 的 while 循环里调用
+ */
+void Playlist_AutoNext_Task(void) {
+    // 如果当前状态是 STOPPED 且不是因为人为操作停下来的
+    // 我们的 audio_driver 会在 MP3 解码完最后一帧后把状态设为 AUDIO_STOPPED
+    if (Audio_GetStatus() == AUDIO_STOPPED) {
+        // 自动播放下一曲
+        Playlist_Next();
+    }
+}
---
+++ User/playlist.h
@@ -0,0 +1,25 @@
+#ifndef __PLAYLIST_H
+#define __PLAYLIST_H
+
+#include "ff.h"
+
+// 最大支持 255 首歌，文件名最大 255 字节
+#define MAX_SONGS 255
+#define MAX_FILENAME_LEN 255
+
+typedef struct {
+    uint16_t total_count;    // 总歌曲数
+    uint16_t current_index;  // 当前播放索引
+    char current_filename[MAX_FILENAME_LEN]; // 当前播的文件名
+} Playlist_t;
+
+// 接口
+FRESULT Playlist_Init(void);             // 扫描 SD 卡建立索引
+FRESULT Playlist_GetFileName(uint16_t index, char* out_name); // 获取第 N 首歌的名字
+void Playlist_Next(void);                // 下一曲
+void Playlist_Prev(void);                // 上一曲
+void Playlist_AutoNext_Task(void);       // 自动切歌检查逻辑
+
+extern Playlist_t g_playlist;
+
+#endif
